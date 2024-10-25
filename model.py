950bbfc34a5e3b0defd9bd2bc883591f05412b11
felix1801 <noiset.lapin@hotmail.fr>
2024-10-25 17:20:05 +0200

    fix deployment

--- model.py
+++ model.py
@@ -1,5 +1,29 @@
+import mlflow
 import numpy as np
 import os
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+class CustomThresholdModel(BaseEstimator, ClassifierMixin):
+    def __init__(self, model, threshold):
+        self.model = model
+        self.threshold = threshold
+
+    def fit(self, X, y):
+        # Pas besoin de ré-entraîner le modèle
+        return self
+
+    def predict(self, X):
+        probas = self.model.predict_proba(X)[:, 1]
+        scores = self.predict_score(X, probas)
+        return scores, probas
+    
+    def predict_score(self, X, probas):
+        return (probas >= self.threshold).astype(int)
+
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+
 
 # Local version
 """
@@ -16,7 +40,6 @@ modeling_path = os.path.join(project_root, 'modeling')
 if modeling_path not in sys.path:
     sys.path.append(modeling_path)
 
-import mlflow
 from pipelines.utils.custom_threshold_model import CustomThresholdModel
 
 model_path = os.path.join(os.path.expanduser("~"), "Desktop", "Ecole", "OpenClassrooms-Projet-7", "modeling", "data", "06_models", "latest")
@@ -26,12 +49,8 @@ model_path = os.path.join(os.path.expanduser("~"), "Desktop", "Ecole", "OpenClas
 model_path = os.path.join(os.getcwd(), "models", "latest")
 
 
-
 # Charger le modèle
 loaded_model = mlflow.sklearn.load_model(model_path)
-
-print(f"Modèle chargé depuis : {model_path}")
-
 
 def predict_score(data_list):
     # Convert list of dictionaries to a numpy array
